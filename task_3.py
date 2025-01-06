81c4f4491f25b0e6e345a5183f799b6abbff982c
Domiff <158328298+Domiff@users.noreply.github.com>
2025-01-06 17:35:02 +0300

    Add files via upload

--- task_3.py
+++ task_3.py
@@ -2,3 +2,19 @@
 # Напишите функцию, которая принимает количество дней от текущей даты и
 # возвращает дату, которая наступит через указанное количество дней. Дополнительно,
 # выведите эту дату в формате YYYY-MM-DD.
+
+
+from datetime import datetime, timedelta
+
+
+def calculate_days(count_days):
+
+    future_date = datetime.now() + timedelta(days=count_days)
+    return future_date.strftime('%Y-%m-%d')
+
+def main():
+    days = int(input("Введите количество дней: "))
+    future_date = calculate_days(days)
+    print(f"Дата через {days} дней: {future_date}")
+
+main()
--- task_4.py
+++ task_4.py
@@ -5,3 +5,35 @@
 # дополнительную информацию о процессе.
 # ● --repeat, если этот параметр установлен, он должен указывать,
 # сколько раз повторить строку в выводе.
+
+
+import argparse
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Скрипт для обработки числа и строки.')
+
+    parser.add_argument('number', type=int, help='Введите целое число')
+    parser.add_argument('string', type=str, help='Введите строку')
+
+    parser.add_argument('--verbose', action='store_true', help='Показать подробную информацию')
+    parser.add_argument('--repeat', type=int, default=1, help='Количество повторений строки')
+
+    args = parser.parse_args()
+
+    number = args.number
+    string = args.string
+    verbose = args.verbose
+    repeat = args.repeat
+
+    if verbose:
+        print(f'Вы ввели число {number}')
+        print(f'Вы ввели строку "{string}"')
+
+    for _ in range(repeat):
+        print(string)
+
+
+if __name__ == "__main__":
+    main()
