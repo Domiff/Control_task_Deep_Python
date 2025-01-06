08bb2c78873943f62273ba44fbb5a5e573ebceb8
Domiff <158328298+Domiff@users.noreply.github.com>
2025-01-06 13:45:38 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -2,3 +2,31 @@
 # Напишите скрипт, который логирует разные типы сообщений в разные файлы.
 # Логи уровня DEBUG и INFO должны сохраняться в debug_info.log, а логи уровня
 # WARNING и выше — в warnings_errors.log.
+
+
+import logging
+
+
+logger1 = logging.getLogger('logger1')
+logger2 = logging.getLogger('logger2')
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+file_handler1 = logging.FileHandler('log/debug_info.log')
+file_handler2 = logging.FileHandler('log/warnings_errors.log')
+
+file_handler1.setFormatter(formatter)
+file_handler2.setFormatter(formatter)
+
+logger1.setLevel(logging.DEBUG)
+logger2.setLevel(logging.WARNING)
+
+logger1.addHandler(file_handler1)
+logger2.addHandler(file_handler2)
+
+logger1.info("Все пучком")
+logger1.debug("Нужно отдебажить код")
+
+logger2.warning("Есть недочетность, но работать будет")
+logger2.error("Нужно исправить код, имеются ошибки")
+logger2.critical("Программа сломана")
--- task_2.py
+++ task_2.py
@@ -2,3 +2,12 @@
 # Напишите скрипт, который получает текущее время и дату, а затем выводит их в
 # формате YYYY-MM-DD HH:MM:SS. Дополнительно, выведите день недели и номер
 # недели в году
+
+
+from datetime import datetime
+
+
+time = datetime.now()
+format_time = time.strftime("Дата %d %B %Y. День недели %A. Время %H:%M:%S. Это %W неделя и %j день года")
+print(time)
+print(format_time)
